d25fb42448510d769e546755cd8ba33bb719a4b1
haitamlamtai <haitam.lamtai@gmail.com>
2020-12-16 17:29:36 +0100

    code explained

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,13 @@
+#loading package
 library(reshape2)
 
 # preprocess labels and features
 activity_labels <- read.csv("data/activity_labels.txt", sep="", header=FALSE)
 activity_labels[,2] <- as.character(activity_labels[,2])
 features <- read.csv("data/features.txt", sep="", header=FALSE)
+
+  #Extracting only the measurements 
+  #on the mean and standard deviation for each measurement.
 wanted_features_rows <- grep(".*mean.*|.*std.*", features[,2])
 wanted_features <- features[wanted_features_rows,2]
 wanted_features = gsub('-mean', 'Mean', wanted_features)
@@ -21,14 +25,17 @@ test_activity <- read.table("data/test/Y_test.txt")
 test_subject <- read.table("data/test/subject_test.txt")
 test <- cbind(test_subject, test_activity, test)
 
+ #Merging the training and the test sets to create one data set
 joint_data <- rbind(train, test)
+
+ #Using descriptive activity names to name the activities in the data set
 colnames(joint_data) <- c("subject", "activity", wanted_features)
 
-# turn activities & subjects into factors
+# turning activities & subjects into factors
 joint_data$activity <- factor(joint_data$activity, levels = activity_labels[,1], labels = activity_labels[,2])
 joint_data$subject <- as.factor(joint_data$subject)
 
-# order data
+# ordering data
 melt_data <- melt(joint_data, id = c("subject", "activity"))
 mean_data <- dcast(melt_data, subject + activity ~ variable, mean)
 
